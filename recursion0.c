ee3f13236d146ea976840b2aa1ec872db7263a51
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-27 10:42:40 +0100

    Added recusion examples

--- recursion0.c
+++ recursion0.c
@@ -11,24 +11,24 @@ int main(void)
 	{
 		printf("Sum of all decimals from %d = %d.\n", i, sum_decimals(i));
 	}
-    printf("\nPrinting sum of decimals from i to 9...\n");
-    for (int i = 0; i < 15; i++)
+	printf("\nPrinting sum of decimals from i to 9...\n");
+	for (int i = 0; i < 15; i++)
 	{
 		print_sum_decimals(i);
 	}
-    printf("\b\b%s%s.\n\nPrinting Decimals in ascending order...\n", "", "");
-    for (int i = 0; i < 11; i++)
+	printf("\b\b%s%s.\n\nPrinting Decimals in ascending order...\n", "", "");
+	for (int i = 0; i < 11; i++)
 	{
 		print_decimals_asc(i);
 		printf("\b\b%s%s.", "", "");
-        printf("\n");
+		printf("\n");
 	}
 	printf("Printing Decimals in descinging order...\n");
-    for (int i = 0; i < 11; i++)
+	for (int i = 0; i < 11; i++)
 	{
 		print_decimals_desc(i);
 		printf("\b\b%s%s.", "", "");
-        printf("\n");
+		printf("\n");
 	}
 }
 
@@ -43,23 +43,23 @@ int sum_decimals(int start)
 void print_sum_decimals(int start)
 {
 	if (start < 10)
-        printf("%d, ", start + sum_decimals(start + 1));
+		printf("%d, ", start + sum_decimals(start + 1));
 }
 
 void print_decimals_asc(int start)
 {
 	if (start < 10)
-    {
-        printf("%d, ", start);
-        print_decimals_asc(start + 1);
-    }
+	{
+		printf("%d, ", start);
+		print_decimals_asc(start + 1);
+	}
 }
 
 void print_decimals_desc(int start)
 {
 	if (start < 10)
-    {
-        print_decimals_desc(start + 1);
-        printf("%d, ", start);
-    }
+	{
+		print_decimals_desc(start + 1);
+		printf("%d, ", start);
+	}
 }
